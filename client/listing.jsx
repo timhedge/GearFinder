dd6c89e33a82a303f3ec447b9cc4fceadc128d3f
Tim Hedge <timwhedge@gmail.com>
2020-04-15 20:35:51 -0500

    adds font styling and reverb logo

--- client/listing.jsx
+++ client/listing.jsx
@@ -8,8 +8,8 @@ const listing = (props) => {
       <td className="listingName">{props.listing.name}</td>
       <td className="listingDesc">{props.listing.source === 'Reverb' ? <div className="longDesc">{ReactHtmlParser(props.listing.description)}</div> : props.listing.description}</td>
       <td className="listingCell">${props.listing.price}</td>
-      <td className="listingCell listingLink"><a href={props.listing.listingUrl} target="_blank">View on {props.listing.source}</a></td>
-      <td className="listingCell listingSource">{props.listing.source}</td>
+      <td className="listingCell"><a className="listingLink" href={props.listing.listingUrl} target="_blank">View on {props.listing.source}</a></td>
+      <td className="listingCell listingSource"><img className="listingSrcImg" src={props.listing.source === 'Reverb' ? "https://images.reverb.com/image/upload/s--N7IPDaUB--/v1496943615/reverb-r-logo-2017_omsytb.png" : "placeholder for ebay"}></img></td>
     </tr>
   )
 }
