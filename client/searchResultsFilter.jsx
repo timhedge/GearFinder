52db5f79d0ea8da439d0989beca9fdb4fcb62332
Tim Hedge <timwhedge@gmail.com>
2020-07-17 15:53:36 -0500

    add price filter mockup

--- client/searchResultsFilter.jsx
+++ client/searchResultsFilter.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 const searchResultsFilter = (props) => {
   return (
-    <div>Filter</div>
+    <form>
+      <div className="form-group">
+        <label>Price</label>
+        <input type="number" className="form-control" placeholder="min" min="0"></input>
+        <input type="number" className="form-control" placeholder="max" min="1"></input>
+      </div>
+      <button type="submit" className="btn btn-secondary">Filter</button>
+    </form>
   )
 }
 
